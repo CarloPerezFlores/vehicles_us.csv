108dc8567b3e758c92b3df00d7f1228ce2e0c9eb
CarloPerezFlores <149033006+CarloPerezFlores@users.noreply.github.com>
2024-01-09 19:09:08 -0600

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import pandas as pd
 import plotly.express as px
 
 # se llama el dataset y se guarda
-data = pd.read_csv('C:/Users/USER/Desktop/Business Analyst Certificate/TRIPLE TEN/PROY 4/vehicles_us.csv')
+try:
+  data = pd.read_csv('C:/Users/USER/Desktop/Business Analyst Certificate/TRIPLE TEN/PROY 4/vehicles_us.csv')
+except FileNotFoundError:
+  st.error('El archivo "vehicles_us.csv" no se encuentra.')
 
 st.header('Anuncios de venta de autos')
 
@@ -13,17 +16,25 @@ scatter_check = st.checkbox('Construir gráfico de dispersión')
 
 # al hacer clic en el botón
 if hist_check:
+  try:
     st.write(
-        'Creación de un histograma para la columna de precios del conjunto de datos de anuncios de venta de autos')
+      'Creación de un histograma para la columna de precios del conjunto de datos de anuncios de venta de autos')
     # se crea un histogrma
-    fig = px.histogram(vehicles, x='price')
+    fig = px.histogram(data, x='price')
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+  except Exception as e:
+    st.error(e)
+
 if scatter_check:
+  try:
     st.write(
-        'Creación de gráfico de dispersión del precio y año del modelo, para el conjunto de datos de anuncios de venta de autos')
+      'Creación de gráfico de dispersión del precio y año del modelo, para el conjunto de datos de anuncios de venta de autos')
     # se crea gráfico de dispersión
-    fig = px.scatter(vehicles, x='model_year', y='price')
+    fig = px.scatter(data, x='model_year', y='price')
     # mostrar un gráfico Plotly interactivo de gráfico de dispersión
     st.plotly_chart(fig, use_container_width=True)
+  except Exception as e:
+    st.error(e)
+
 
